43b4c491299e3dbe83fe620b7f3bd798ca7e44bf
dinesh <dgoud99120@gmail.com>
2023-08-23 23:04:43 +0530

    product

--- app/api/product/[id].js
+++ app/api/product/[id].js
@@ -1,28 +1,17 @@
-// import connectDB from "@/backend/config/connectDB";
-// import Products from "@/backend/models/product";
-// import { NextResponse } from "next/server";
+import connectDB from "@/backend/config/connectDB";
+import Products from "@/backend/models/product";
+import { NextResponse } from "next/server";
 
-// export async function GET(request) {
-//     await connectDB();
-//     try{
-//       const product = await Products.findById(request.query.id);
-//       if(!product) {
-//         NextResponse.json(
-//           { message: 'Product not found'},
-//           {status: 404}
-//         );
-//       }
-//       NextResponse.json(
-//         {product, message: 'Product found!'},
-//         {status: 200}
-//       )
-  
-//     } catch(error) {
-//       console.error("Error:", error);
-//       return NextResponse.json( 
-//         { message: "Internal server error" },
-//         { status: 500 }
-//       );
-//     }
-//   }
-  
+export async function GET(request) {
+  await connectDB();
+  try {
+    const product = await Products.findById(request.query.id);
+    if (!product) {
+      return NextResponse.error('Product not found', { status: 404 });
+    }
+    return NextResponse.json({ product, message: 'Product found!' }, { status: 200 });
+  } catch (error) {
+    console.error("Error:", error);
+    return NextResponse.error('Internal server error', { status: 500 });
+  }
+}
